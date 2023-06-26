df7c497338ab4952e9c1ca07188df126d415e863
murat metin akkus <metin.akku2017@yandex.com>
2023-06-26 22:13:56 +0200

    Team Practice

--- src/test/java/tests/teamPractice/P06_IFrame.java
+++ src/test/java/tests/teamPractice/P06_IFrame.java
@@ -1,4 +1,4 @@
-package tests.day08_iFrame_windows_actionsClass;
+package tests.teamPractice;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -7,25 +7,30 @@ import utilities.TestBase;
 
 import java.util.List;
 
-public class C02_IFrame extends TestBase {
+public class P06_IFrame extends TestBase {
     @Test
     public void test01(){
-
-        //1) /http://demo.guru99.com/test/guru99home sitesine gidiniz
+        //1) http://demo.guru99.com/test/guru99home/ sitesine gidiniz
         driver.get("http://demo.guru99.com/test/guru99home/");
+       // WebElement cookiesElement = driver.findElement(By.xpath("//span[text()='Accept All']"));
+       // if (cookiesElement.isDisplayed()){
+       //     cookiesElement.click();
+       // }else {
+       //     System.out.println("cookies gozukmedi");
+       // }
         //2) sayfadaki iframe sayısını bulunuz.
-        List<WebElement> iFrameList = driver.findElements(By.tagName("//iframe"));
-        System.out.println("iFrameList = " + iFrameList.size() + " numbers");
+        List<WebElement> iFrameElement = driver.findElements(By.tagName("iframe"));
+        System.out.println("iFrameElement.size() = " + iFrameElement.size());
         //3) ilk iframe'deki (Youtube) play butonuna tıklayınız.
         WebElement youtubeIframe = driver.findElement(By.xpath("//iframe[@wmode='transparent']"));
         driver.switchTo().frame(youtubeIframe);
         driver.findElement(By.xpath("//button[@aria-label='Play']")).click();
-        stop(5);
         //4) ilk iframe'den çıkıp ana sayfaya dönünüz
         driver.switchTo().defaultContent();
         //5) ikinci iframe'deki (Jmeter Made Easy) linke
         //(https://www.guru99.com/live-selenium-project.html) tıklayınız
         WebElement secondIFrameElement = driver.findElement(By.id("a077aa5e"));
         secondIFrameElement.click();
+
     }
 }
